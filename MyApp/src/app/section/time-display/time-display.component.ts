e77857573f17c06a65d3006dad45457e4827b5a5
woowoni <ewoowon.2@gmail.com>
2023-03-04 19:24:26 +0900

    angular-cli install

---
+++ MyApp/src/app/section/time-display/time-display.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-time-display',
+  templateUrl: './time-display.component.html',
+  styleUrls: ['./time-display.component.scss']
+})
+export class TimeDisplayComponent {
+
+}
